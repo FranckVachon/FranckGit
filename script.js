5ddcd2e816815c217e15813fd075567af937627a
FranckVachon <francisvachon.it@gmail.com>
2016-08-10 19:19:13 -0400

    removing testing stuff, update general template

--- script.js
+++ script.js
@@ -11,26 +11,33 @@ Once that's done, I would like Flask to return a error/success code to D3. Not w
 Then, the DB for D3 is updated - that's the data.csv
 */
 
+//Let's start with some simple sample graphs
+
 //##TODO: have the URL for Flask be setup in a variables.csv file instead of hardcoded.
 //##TODO: get the error/success response from Flask to D3 to work
 
 
-//################## WORKING SNIPPET BELOW FOR FLASK CALLS
-/*
-Flask must be up & running @ URL. kobod3.py returns an error/success code (TBD), which flask returns as well to D3. It is shown & as "answer". Will allow error controls in case update requests fails, could try X times.
-*/
-// var URL = "http://127.0.0.1:5000/test"
-// d3.text(URL, "text/plain", function(error, answer){
-//     d3.select("body")
-//         .append("div")
-//         .text(answer);
-// })
-//################## WORKING SNIPPET  FOR FLASK CALLS - THE RESPONSE FROM FLASK CAN'T YET BE READ
 
-//############# WORKING SNIPPET FOR DYNAMICALLY POPULATED DROPDOWN
+/*
+//Flask must be up & running @ URL. kobod3.py returns an error/success code (TBD), 
+//which flask returns as well to D3. 
+//It is shown & as "answer". Will allow error controls in case update requests
+// fails, could try X times.
+
+var URL = "http://127.0.0.1:5000/test"
+d3.text(URL, "text/plain", function(error, answer){
+    d3.select("body")
+    .append("div")
+    .text(answer);
+    })
+*/
+
+
+
+// #dropdown
 /*
 Populates the dropdown according to whatever values are listed in dropdown.csv
-*/
+
 d3.csv("dropdowns.csv", function(error, d){
     var options = [];
     if (error) throw error;
@@ -46,6 +53,6 @@ d3.csv("dropdowns.csv", function(error, d){
         .append("option")
         .text(function(d){return d;})
     });
-//############# WORKING SNIPPET FOR DYNAMICALLY POPULATED DROPDOWN
+*/
 
 
